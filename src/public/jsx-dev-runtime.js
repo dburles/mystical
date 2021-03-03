f8507b3a4be58e08d4e808b1f2a8c4075362b4e6
David Burles <dburles@gmail.com>
2021-03-03 15:17:05 +1100

    Update jsx-dev-runtime.js

--- src/public/jsx-dev-runtime.js
+++ src/public/jsx-dev-runtime.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const { jsxDEV } = require('@emotion/react/jsx-dev-runtime');
+const { jsxDEV: emotionJsxDev } = require('@emotion/react/jsx-dev-runtime');
 const createJsxFn = require('../private/createJsxFn');
 
-const jsx = createJsxFn(jsxDEV);
+const jsxDEV = createJsxFn(emotionJsxDev);
 
-module.exports = jsx;
+module.exports = { jsxDEV };
