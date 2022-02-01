110fc37e57c3c7444313eda1030e104e810b433d
David Burles <dburles@gmail.com>
2022-02-01 22:39:30 +1100

    improve naming

--- src/private/createJsxFn.js
+++ src/private/createJsxFn.js
@@ -3,13 +3,13 @@
 const transformStyles = require('./transformStyles');
 
 const createJsxFn = (jsxFn) => {
-  return (type, props, ...children) => {
-    const { css, ...rest } = props || {};
+  return (type, props, ...rest) => {
+    const { css, ...restProps } = props || {};
 
     return jsxFn(
       type,
-      css ? { css: transformStyles(css), ...rest } : props,
-      ...children
+      css ? { css: transformStyles(css), ...restProps } : props,
+      ...rest
     );
   };
 };
