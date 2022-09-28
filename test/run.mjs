b142aa2a85aebea6aee72dc0e59029b7e23620a7
David Burles <dburles@gmail.com>
2022-09-28 10:36:16 +1000

    update deps

--- test/run.mjs
+++ test/run.mjs
@@ -1,4 +1,5 @@
-import TestDirector from "test-director";
+/* eslint-disable react-hooks/rules-of-hooks */
+import TestDirector from "test-director/TestDirector.mjs";
 import customProperties from "./customProperties.test.mjs";
 import mergeModifiers from "./mergeModifiers.test.mjs";
 import transformStyles from "./transformStyles.test.mjs";
