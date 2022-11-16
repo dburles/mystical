4f64c167539526ac2d8ebc16d74a50e321b11379
David Burles <dburles@gmail.com>
2022-11-16 13:18:24 +1100

    correct type declarations

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,11 @@
-export * from "@emotion/core";
+declare module "mystical/Global.js" {
+  export { Global as default } from "@emotion/react/types";
+}
+
+declare module "mystical/jsx-dev-runtime" {
+  export * from "@emotion/react/types/jsx-dev-runtime";
+}
+
+declare module "mystical/jsx-runtime" {
+  export * from "@emotion/react/types/jsx-runtime";
+}
