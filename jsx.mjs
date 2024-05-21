e06546586136aa59f495ec147533dd23b5d89565
David Burles <dburles@gmail.com>
2024-05-21 14:02:53 +1000

    replaced with createElement

--- jsx.mjs
+++
@@ -1,6 +0,0 @@
-import { jsx as emotionJsx } from "@emotion/react";
-import wrapCreateElement from "./private/wrapCreateElement.mjs";
-
-const jsx = wrapCreateElement(emotionJsx);
-
-export default jsx;
