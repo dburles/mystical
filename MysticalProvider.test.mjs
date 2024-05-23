9cd6bb847feb5bce1c128c7965fb47fa98d947ad
David Burles <dburles@gmail.com>
2024-05-23 10:46:36 +1000

    drop deprecated react-test-renderer

--- MysticalProvider.test.mjs
+++ MysticalProvider.test.mjs
@@ -1,35 +1,33 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import assert from "node:assert/strict";
-import { act, create } from "react-test-renderer";
 import MysticalProvider from "./MysticalProvider.mjs";
 import test from "node:test";
 import createElement from "./createElement.mjs";
+import { render } from "@testing-library/react";
+import "global-jsdom/register";
 
 test("MysticalProvider", async (t) => {
   await t.test("theme object is optional", () => {
-    let testRenderer;
-    act(() => {
-      testRenderer = create(
-        createElement(
-          MysticalProvider,
-          {
-            theme: () => {
-              return {};
-            },
+    const { getByText } = render(
+      createElement(
+        MysticalProvider,
+        {
+          theme: () => {
+            return {};
           },
-          createElement(
-            "div",
-            {
-              css: { color: "red" },
-            },
-            "Red text",
-          ),
-        ),
-      );
-    });
+        },
+        createElement(
+          "div",
+          {
+            css: { color: "red" },
+          },
+          "Red text"
+        )
+      )
+    );
 
-    assert.ok(testRenderer);
-    const json = testRenderer.toJSON();
-    assert.equal(json[2].props.className, "css-tokvmb");
+    const element = getByText("Red text");
+
+    assert.equal(element.className, "css-tokvmb");
   });
 });
--- useTheme.test.mjs
+++ useTheme.test.mjs
@@ -1,31 +1,30 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import assert from "node:assert/strict";
 import useTheme from "./useTheme.mjs";
-import { act, create } from "react-test-renderer";
 import { createElement } from "react";
 import ReactHookTest from "./test-utils/ReactHookTest.mjs";
 import theme from "./test-utils/theme.mjs";
 import MysticalProvider from "./MysticalProvider.mjs";
 import test from "node:test";
+import { render } from "@testing-library/react";
+import "global-jsdom/register";
 
 test("useTheme", async (t) => {
   await t.test("reads values", () => {
     const hookResults = [];
 
-    act(() => {
-      create(
-        createElement(
-          MysticalProvider,
-          { theme },
-          createElement(ReactHookTest, {
-            hook() {
-              return useTheme("colors", "emerald.500");
-            },
-            results: hookResults,
-          }),
-        ),
-      );
-    });
+    render(
+      createElement(
+        MysticalProvider,
+        { theme },
+        createElement(ReactHookTest, {
+          hook() {
+            return useTheme("colors", "emerald.500");
+          },
+          results: hookResults,
+        })
+      )
+    );
 
     assert.equal(hookResults[0], theme.colors.emerald["500"]);
   });
@@ -33,20 +32,18 @@ test("useTheme", async (t) => {
   await t.test("retains values that aren't found", () => {
     const hookResults = [];
 
-    act(() => {
-      create(
-        createElement(
-          MysticalProvider,
-          { theme },
-          createElement(ReactHookTest, {
-            hook() {
-              return useTheme("colors", "emerald.1000");
-            },
-            results: hookResults,
-          }),
-        ),
-      );
-    });
+    render(
+      createElement(
+        MysticalProvider,
+        { theme },
+        createElement(ReactHookTest, {
+          hook() {
+            return useTheme("colors", "emerald.1000");
+          },
+          results: hookResults,
+        })
+      )
+    );
 
     assert.equal(hookResults[0], "emerald.1000");
   });
@@ -54,20 +51,18 @@ test("useTheme", async (t) => {
   await t.test("self referencing values are transformed", () => {
     const hookResults = [];
 
-    act(() => {
-      create(
-        createElement(
-          MysticalProvider,
-          { theme },
-          createElement(ReactHookTest, {
-            hook() {
-              return useTheme("colors", "brand.primary");
-            },
-            results: hookResults,
-          }),
-        ),
-      );
-    });
+    render(
+      createElement(
+        MysticalProvider,
+        { theme },
+        createElement(ReactHookTest, {
+          hook() {
+            return useTheme("colors", "brand.primary");
+          },
+          results: hookResults,
+        })
+      )
+    );
 
     assert.equal(hookResults[0], theme.colors.emerald["500"]);
   });
