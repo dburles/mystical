831a4aa98d319833eadead7853290f1c7ae0348b
David Burles <dburles@gmail.com>
2024-10-14 15:43:13 +1100

    export types.mjs

---
+++ types.mjs
@@ -0,0 +1,10 @@
+/** @import { MysticalCSSProp } from "./jsx-namespace.mjs" */
+
+/** @typedef {MysticalCSSProp} MysticalCSSProp */
+
+/**
+ * @template T
+ * @typedef {{ [key: string]: NestedObject<T> | T }} NestedObject<T>
+ */
+
+/** @typedef {NestedObject<MysticalCSSProp>} Modifiers */
